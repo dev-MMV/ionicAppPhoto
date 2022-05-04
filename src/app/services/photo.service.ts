25a0015e4855563755d1ae1fd246087b7f2036b8
dev-mmv <mm.valente@huna.io>
2022-05-04 14:58:14 +0200

    add store photo

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Camera, CameraResultType, CameraSource, Photo} from '@capacitor/camera';
 import {from, Observable, ReplaySubject, Subject} from 'rxjs';
-import {startWith} from "rxjs/operators";
+import {map, startWith, switchMap} from "rxjs/operators";
+import {Directory, Filesystem} from "@capacitor/filesystem";
 
 export interface UserPhoto {
   filepath: string;
@@ -40,5 +41,66 @@ export class PhotoService {
       });
     }
     this.emitPhotos.next(this.storedPhoto.slice());
+  }
+
+  private convertBlobToBase64(blob: Blob): Observable<string | ArrayBuffer> {
+    return new Observable((subscriber) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onerror = (error) => {
+        subscriber.error(error);
+        subscriber.complete();
+        subscriber.unsubscribe();
+      };
+      reader.onload = () => {
+        subscriber.next(reader.result);
+        subscriber.complete();
+        subscriber.unsubscribe();
+      };
+    });
+  }
+
+  private convertArrayBufferToString(arrayBuffer: ArrayBuffer): string {
+    return new TextDecoder('utf-8').decode(arrayBuffer);
+  }
+
+  private readAsBase64(photo: Photo): Observable<string> {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const blob = fetch(photo.webPath).then(response => response.blob());
+    return from(blob).pipe(
+      switchMap((blobFile) => this.convertBlobToBase64(blobFile)),
+      map((val) => {
+        if (typeof val == 'string') {
+          return val;
+        } else {
+          return this.convertArrayBufferToString(val);
+        }
+      })
+    );
+  }
+
+  private savePicture(photo: Photo): Observable<UserPhoto> {
+    // Convert photo to base64 format, required by Filesystem API to save
+    // Write the file to the data directory
+    const savedFile = (name: string, base64: string,) => Filesystem.writeFile({
+      path: name,
+      data: base64,
+      directory: Directory.Data
+    });
+
+    const fileName = new Date().getTime() + '.jpeg';
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return this.readAsBase64(photo).pipe(
+      switchMap((base64) => from(savedFile(fileName, base64))),
+      map(photoStored => {
+        const userPhoto: UserPhoto = {
+          filepath: fileName,
+          webviewPath: photo.webPath
+        };
+        return userPhoto;
+      })
+    );
+
   }
 }
